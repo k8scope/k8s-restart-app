5d6d3d640754b2f64dc7b81349fd69e11fba8066
leonsteinhaeuser <leon.steinhaeuser@tagesspiegel.de>
2024-10-09 15:54:37 +0200

    feat(e2e): add test case for restart service

--- _e2e/e2e_test.go
+++ _e2e/e2e_test.go
@@ -2,12 +2,14 @@ package e2e
 
 import (
 	"encoding/json"
+	"fmt"
 	"io"
 	"net/http"
 	"os"
 	"testing"
 
 	"github.com/k8scope/k8s-restart-app/internal/config"
+	"github.com/k8scope/k8s-restart-app/internal/k8s"
 	"github.com/k8scope/k8s-restart-app/internal/utils"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
@@ -64,5 +66,51 @@ func Test_ListApplications(t *testing.T) {
 	if len(cfg.Services) != 3 {
 		t.Fatalf("expected 3 services, got %d", len(cfg.Services))
 		return
+	}
+}
+
+func Test_Restart(t *testing.T) {
+	var (
+		testCases = []struct {
+			Name               string
+			Target             k8s.KindNamespaceName
+			ExpectedStatusCode int
+		}{
+			{
+				Name: "non existing service",
+				Target: k8s.KindNamespaceName{
+					Kind:      "Deployment",
+					Namespace: "default",
+					Name:      "nginx",
+				},
+				ExpectedStatusCode: http.StatusNotFound,
+			},
+			{
+				Name: "successful restart",
+				Target: k8s.KindNamespaceName{
+					Kind:      "Deployment",
+					Namespace: "test-1",
+					Name:      "ngx-1",
+				},
+				ExpectedStatusCode: http.StatusNotFound,
+			},
+		}
+	)
+
+	for _, tc := range testCases {
+		t.Run(tc.Name, func(tt *testing.T) {
+			rsp, err := http.Post(fmt.Sprintf("%s/api/v1/service/%s/%s/%s", serviceAddress, tc.Target.Kind, tc.Target.Namespace, tc.Target.Name), "application/json", nil)
+			if err != nil {
+				t.Fatalf("Test_Restart(): failed to restart deployment: %v", err)
+				return
+			}
+
+			if rsp.StatusCode != tc.ExpectedStatusCode {
+				t.Fatalf("Test_Restart(): expected status code %d, got %d", tc.ExpectedStatusCode, rsp.StatusCode)
+				return
+			}
+
+			// TODO: check if the service is locked during the restart
+		})
 	}
 }
