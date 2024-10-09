1a07f43366e2366581b60bd1440f557bd441d0c3
leonsteinhaeuser <leon.steinhaeuser@tagesspiegel.de>
2024-10-09 17:17:03 +0200

    feat(e2e): add tests for status http and websocket requests

--- _e2e/e2e_test.go
+++ _e2e/e2e_test.go
@@ -1,15 +1,20 @@
 package e2e
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
+	"net/url"
 	"os"
 	"testing"
+	"time"
 
+	"github.com/gorilla/websocket"
 	"github.com/k8scope/k8s-restart-app/internal/config"
 	"github.com/k8scope/k8s-restart-app/internal/k8s"
+	"github.com/k8scope/k8s-restart-app/internal/ledger"
 	"github.com/k8scope/k8s-restart-app/internal/utils"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
@@ -111,6 +116,109 @@ func Test_Restart(t *testing.T) {
 			}
 
 			// TODO: check if the service is locked during the restart
+		})
+	}
+}
+
+func Test_Status_HTTP(t *testing.T) {
+	var (
+		testCases = []struct {
+			Name               string
+			ExpectedStatusCode int
+		}{
+			{
+				Name:               "expect 400, because the request does not initiate a websocket connection",
+				ExpectedStatusCode: http.StatusBadRequest,
+			},
+		}
+	)
+
+	for _, tc := range testCases {
+		t.Run(tc.Name, func(tt *testing.T) {
+			rsp, err := http.Get(fmt.Sprintf("%s/api/v1/service/status", serviceAddress))
+			if err != nil {
+				t.Fatalf("Test_Status_HTTP(): failed to restart deployment: %v", err)
+				return
+			}
+
+			if rsp.StatusCode != tc.ExpectedStatusCode {
+				t.Fatalf("Test_Status_HTTP(): expected status code %d, got %d", tc.ExpectedStatusCode, rsp.StatusCode)
+				return
+			}
+		})
+	}
+}
+
+func Test_Status_WebSocket(t *testing.T) {
+	var (
+		testCases = []struct {
+			Name               string
+			ExpectedStatusCode int
+			DeadlineSec        int
+			ExpectMessage      bool
+		}{
+			{
+				Name:               "expect connection switch",
+				ExpectedStatusCode: http.StatusSwitchingProtocols,
+				DeadlineSec:        2,
+			},
+		}
+	)
+
+	// parse URL
+	urli, err := url.Parse(fmt.Sprintf("%s/api/v1/service/status", serviceAddress))
+	if err != nil {
+		t.Fatalf("Test_Status_WebSocket(): failed to parse URL: %v", err)
+		return
+	}
+
+	if urli.Scheme == "https" {
+		urli.Scheme = "wss"
+	} else {
+		urli.Scheme = "ws"
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.Name, func(tt *testing.T) {
+			conn, rsp, err := websocket.DefaultDialer.Dial(urli.String(), nil)
+			if err != nil {
+				t.Fatalf("Test_Status_WebSocket(): failed to connect to websocket: %v", err)
+				return
+			}
+
+			if rsp.StatusCode != tc.ExpectedStatusCode {
+				t.Fatalf("Test_Status(): expected status code %d, got %d", tc.ExpectedStatusCode, rsp.StatusCode)
+				return
+			}
+
+			ctx, cf := context.WithTimeout(context.Background(), time.Second*time.Duration(tc.DeadlineSec))
+			defer cf()
+
+			for {
+				select {
+				case <-ctx.Done():
+					t.Log("Test_Status(): deadline exceeded")
+					return
+				default:
+					_, bts, err := conn.ReadMessage()
+					if err != nil {
+						t.Fatalf("Test_Status(): failed to read message: %v", err)
+						return
+					}
+
+					objs := &ledger.ObjectStatus{}
+					err = json.Unmarshal(bts, objs)
+					if err != nil {
+						t.Fatalf("Test_Status(): failed to parse message: %v", err)
+						return
+					}
+
+					if (objs == nil) && tc.ExpectMessage {
+						t.Fatalf("Test_Status(): expected message, got nil")
+						return
+					}
+				}
+			}
 		})
 	}
 }
